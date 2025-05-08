d6019812f0811a8698dc3c0fab52170494c18d5e
Russell de Pina <rdepina@microsoft.com>
2025-05-08 13:14:23 -0500

    add blog content

---
+++ aks-blog/blog/authors/index.js
@@ -0,0 +1,49 @@
+// // Authors map for AKS Blog
+
+// // CommonJS format for better compatibility with Docusaurus
+// const quentinPetraroia = require('./quentin-petraroia.json');
+// const brianRedmond = require('./brian-redmond.json');
+// const pavneetAhluwalia = require('./pavneet-ahluwalia.json');
+// const paulYu = require('./paul-yu.json');
+// const ryanZhang = require('./ryan-zhang.json');
+// const jackJiang = require('./jack-jiang.json');
+// const allyFord = require('./ally-ford.json');
+// const gemmaTipper = require('./gemma-tipper.json');
+// const sanketBakshi = require('./sanket-bakshi.json');
+// const bridgetKromhout = require('./bridget-kromhout.json');
+// const ernestWong = require('./ernest-wong.json');
+// const jorgePalma = require('./jorge-palma.json');
+// const kennethKilty = require('./kenneth-kilty.json');
+// const cocoWang = require('./coco-wang.json');
+// const sachiDesai = require('./sachi-desai.json');
+// const bobMital = require('./bob-mital.json');
+// const steveGriffith = require('./steve-griffith.json');
+// const surajDeshmukh = require('./suraj-deshmukh.json');
+// const saurabhSharma = require('./saurabh-sharma.json');
+// const kaarthikeyanSubramanian = require('./kaarthikeyan-subramanian.json');
+// const sarahZhou = require('./sarah-zhou.json');
+
+// // Export using CommonJS format
+// module.exports = {
+//   'quentin-petraroia': quentinPetraroia,
+//   'brian-redmond': brianRedmond,
+//   'pavneet-ahluwalia': pavneetAhluwalia,
+//   'paul-yu': paulYu,
+//   'ryan-zhang': ryanZhang,
+//   'jack-jiang': jackJiang,
+//   'ally-ford': allyFord,
+//   'gemma-tipper': gemmaTipper,
+//   'sanket-bakshi': sanketBakshi,
+//   'bridget-kromhout': bridgetKromhout,
+//   'ernest-wong': ernestWong,
+//   'jorge-palma': jorgePalma,
+//   'kenneth-kilty': kennethKilty,
+//   'coco-wang': cocoWang,
+//   'sachi-desai': sachiDesai,
+//   'bob-mital': bobMital,
+//   'steve-griffith': steveGriffith,
+//   'suraj-deshmukh': surajDeshmukh,
+//   'saurabh-sharma': saurabhSharma,
+//   'kaarthikeyan-subramanian': kaarthikeyanSubramanian,
+//   'sarah-zhou': sarahZhou,
+// };
